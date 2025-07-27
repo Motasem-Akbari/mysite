03f684898e8b5821b863415a5b1f0c390fb8e79c
Motasem Akbari <motasemakbari@gmail.com>
2025-07-27 15:04:10 +0330

    rendered HTML files
    Added templates and rendered HTML files through views in the website app.

--- website/views.py
+++ website/views.py
@@ -3,16 +3,11 @@ from django.http import HttpResponse, JsonResponse
 
 
 def index_view(request):
-    return HttpResponse("<h1>Home page</h1>"
-                        "(about-view)\n"
-                        "(contect-view)")
+    return render(request,'website/index.html')
 
 
 def abuot_view(request):
-    return HttpResponse("<h1>About page</h1>"
-                        "(contect-view)")
-
+    return render(request, 'website/abuot.html')
 
 def contact_view(request):
-    return HttpResponse("<h1>Contact page</h1>"
-                        "(abuot-view)")
+    return render(request, 'website/contact.html')
