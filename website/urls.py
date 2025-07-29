2385874b90fc7e83f52e4b91f2ba153ae3cdf2c9
Motasem Akbari <motasemakbari@gmail.com>
2025-07-29 21:28:27 +0330

    Setup base template with static files and separate blog app
    - Created a base.html file including header and footer components
    - Implemented {% block content %} and extended it in all pages
    - Linked static files (images, CSS, etc.) using Django's {% static %} tag
    - Added and configured views and URL paths for index, profile, and details pages
    - Created a separate app named 'blog' for blog-related content
    - Fixed ModuleNotFoundError in INSTALLED_APPS by correcting the app list formatting

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from website.views import *
 
+app_name= 'website'
+
 urlpatterns = [
-    path('', index_view),
-    path('about-view', abuot_view),
-    path('contect-view', contact_view)
+    path('', index,name='home'),
+    path('about', about,name='about'),
+    path('contact', contact,name='contact'),
+    path('elements', elements,name='elements'),
+    path('packages', packages,name='packages')
 ]
--- website/views.py
+++ website/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 
 
-def index_view(request):
+def index(request):
     return render(request,'website/index.html')
 
+def about(request):
+    return render(request, 'website/about.html')
 
-def abuot_view(request):
-    return render(request, 'website/abuot.html')
+def contact(request):
+    return render(request, 'website/contact.html')
 
-def contact_view(request):
+def elements(request):
+    return render(request, 'website/elements.html')
+
+def packages(request):
+    return render(request, 'website/packages.html')
+
+def contact(request):
     return render(request, 'website/contact.html')
