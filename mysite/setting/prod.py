703466a0234dd65e36f03c84759dde24aa2cf8e7
Motasem Akbari <motasemakbari@gmail.com>
2025-08-16 18:10:22 +0330

    Add separate settings for development and production
    - Created a new settings package with `__init__.py`, `dev.py`, and `prod.py`
    - `dev.py` will contain development-specific configurations
    - `prod.py` will contain production-specific configurations
    - Updated project structure to support multiple environments

---
+++ mysite/setting/prod.py
@@ -0,0 +1,41 @@
+from mysite.settings import *
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-=wh*_!wig)(421!zzq(m*=7=$(k1zw=jlu@rt@gf^=kifupu%s'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+# INSTALLED_APPS = []
+
+# sites framework
+SITE_ID = 2
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+
+STATIC_ROOT = BASE_DIR / 'static/'
+MEDIA_ROOT = BASE_DIR / 'MEDIA/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "statics",
+]
+
+
+# CSRF_COOKIE_SECURE= True
--- mysite/settings.py
+++ mysite/settings.py
@@ -16,18 +16,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=wh*_!wig)(421!zzq(m*=7=$(k1zw=jlu@rt@gf^=kifupu%s'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -52,9 +40,6 @@ INSTALLED_APPS = [
     'accounts'
 ]
 
-
-# sites framework
-SITE_ID = 2
 
 # robots
 ROBOTS_USE_HOST = True
@@ -100,17 +85,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -146,14 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static/'
-
 MEDIA_URL = 'MEDIA/'
-MEDIA_ROOT = BASE_DIR / 'MEDIA/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / "statics",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
